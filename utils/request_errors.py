f7c78a96f99ba989baacf1c90496cd48b3b5840f
tjpin <lazarusmwangi1@gmail.com>
2023-04-02 11:49:44 +0300

    added fav icon

--- utils/request_errors.py
+++ utils/request_errors.py
@@ -10,6 +10,6 @@ def error_403(request, exception):
 def error_404(request, exception):
     return render(request,'exceptions/404.html', {})
     
-def error_500(request, exception):
+def error_500(request):
     return render(request,'exceptions/500.html', {})
 
